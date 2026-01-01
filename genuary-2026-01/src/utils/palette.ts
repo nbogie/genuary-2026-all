cd95577405d47592a36117e2f81eab62d9292fbb
nbogie <neillbogie@googlemail.com>
2026-01-01 20:03:57 +0000

    palette tidy

--- genuary-2026-01/src/utils/palette.ts
+++ genuary-2026-01/src/utils/palette.ts
@@ -4,14 +4,17 @@ export function randomColour(): p5.Color {
 }
 
 export function getPalette() {
-  // return {
-  //   colors: ["rgb(85.442% 38.496% 6.9659%)", "rgb(23.924% 20.801% 22.311%)"],
-  //   background: "rgb(14.968% 13.28% 12.473%)",
-  // };
-  return {
-    colors: ["white", "linen"],
-    background: "#1e1e1e",
-  };
+  const palettes = [
+    {
+      colors: ["rgb(85.442% 38.496% 6.9659%)", "rgb(23.924% 20.801% 22.311%)"],
+      background: "rgb(14.968% 13.28% 12.473%)",
+    },
+    {
+      colors: ["white", "linen"],
+      background: "#1e1e1e",
+    },
+  ];
+  return palettes[1];
 }
 
 export function getBackgroundColour(): p5.Color {
