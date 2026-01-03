1b4744a995fa9bba74193e480d4d40e10ccd118a
nbogie <neillbogie@googlemail.com>
2026-01-03 03:46:04 +0000

    small chance each frame of agents being given inputs to change state

--- genuary-2026-02/src/Agent.ts
+++ genuary-2026-02/src/Agent.ts
@@ -2,28 +2,41 @@ import p5 from "p5";
 import { createStateMachine, type StateMachine, type Transition } from "./stateMachine.ts";
 import { collect, randomScreenPos } from "./utils.ts";
 
+export interface Agent {
+  pos: p5.Vector;
+  dest: p5.Vector;
+  facing: number;
+  phase: number;
+  machine: StateMachine<AgentState, AgentInput>;
+  size: number;
+}
+
 export function drawAgent(agent: Agent) {
+  if (agent.dest) {
+    push();
+    translate(agent.dest);
+    noFill();
+    stroke(agent.machine.currentState.name === "seek" ? "magenta" : "gray");
+    const targetDiam = map(sin(agent.phase + millis() / 100), -1, 1, 5, 15, true);
+    circle(0, 0, targetDiam);
+    pop();
+  }
   push();
   translate(agent.pos);
-
+  rotate(agent.facing);
   rectMode(CENTER);
   fill("linen");
-  rect(0, 0, 100, 30);
+  rect(0, 0, agent.size, agent.size * 0.3);
 
   textAlign(CENTER, CENTER);
   fill(30);
+  textSize((16 * agent.size) / 80);
   text(agent.machine.currentState.name, 0, 0);
   pop();
 }
 
 export function createAgents(numAgents: number) {
   return collect(numAgents, createOneAgent);
-}
-
-export interface Agent {
-  pos: p5.Vector;
-  dest: p5.Vector;
-  machine: StateMachine<AgentState, AgentInput>;
 }
 function createOneAgent(): Agent {
   //TODO: these should be readonly
@@ -32,16 +45,20 @@ function createOneAgent(): Agent {
   const acquiringTargetState: AgentState = { name: "acquiringTarget" } as const;
 
   const transitions: Transition<AgentState, AgentInput>[] = [
-    { from: startingState, input: { id: "click" }, to: seekState },
-    { from: seekState, input: { id: "startReassign" }, to: acquiringTargetState },
-    { from: acquiringTargetState, input: { id: "click" }, to: seekState },
+    { from: startingState, input: { id: "unleash" }, to: seekState },
+    { from: startingState, input: { id: "startReassign" }, to: acquiringTargetState },
+    { from: seekState, input: { id: "noteArrival" }, to: startingState },
+    { from: acquiringTargetState, input: { id: "unleash" }, to: seekState },
   ];
   const machine = createStateMachine(startingState, transitions);
 
   const agent: Agent = {
     pos: randomScreenPos(),
     dest: randomScreenPos(),
+    facing: random(TWO_PI),
     machine,
+    phase: random(TWO_PI),
+    size: random(30, 50),
   };
 
   (agent as any).machine = machine;
@@ -52,9 +69,28 @@ export function updateAgent(agent: Agent): void {
   switch (agent.machine.currentState.name) {
     case "idle":
       agent.pos.add(p5.Vector.random2D().mult(0.2));
+      if (random() < 0.001) {
+        agent.machine.applyInput({ id: "startReassign" });
+      }
+
       break;
     case "seek":
       agent.pos.lerp(agent.dest, 0.1);
+      turnAgentTowardsDest(agent);
+      if (agent.pos.dist(agent.dest) < 20) {
+        agent.machine.applyInput({ id: "noteArrival" });
+        agent.dest = randomScreenPos();
+      }
+      break;
+    case "acquiringTarget":
+      if (random() < 0.02) {
+        agent.dest = randomScreenPos();
+      }
+      if (random() < 0.0001) {
+        agent.machine.applyInput({ id: "unleash" });
+      }
+      turnAgentTowardsDest(agent);
+
       break;
 
     default:
@@ -66,8 +102,16 @@ export type AgentState = { name: "idle" } | { name: "seek" } | { name: "acquirin
 
 export type AgentInput =
   | {
-      id: "click";
+      id: "unleash";
     }
   | {
       id: "startReassign";
+    }
+  | {
+      id: "noteArrival";
     };
+
+function turnAgentTowardsDest(agent: Agent) {
+  const vecToDest = p5.Vector.sub(agent.dest, agent.pos);
+  agent.facing = lerp(agent.facing, vecToDest.heading(), 0.03);
+}
--- genuary-2026-02/src/animation.ts
+++ genuary-2026-02/src/animation.ts
@@ -1,12 +1,11 @@
 import { createAgents, drawAgent, updateAgent, type Agent, type AgentInput } from "./Agent.ts";
-import { randomScreenPos } from "./utils.ts";
 
 export interface GlobalState {
   agents: Agent[];
 }
 
 export function setupAnimation() {
-  const agents = createAgents(5);
+  const agents = createAgents(50);
   const state = { agents };
   return state;
 }
@@ -23,13 +22,8 @@ export function animationHandleMousePressed(state: GlobalState) {
   state.agents.forEach((ag: Agent) => {
     const d = ag.pos.dist(createVector(mouseX, mouseY));
     if (d < 70) {
-      const input: AgentInput = keyIsDown(SHIFT) ? { id: "startReassign" } : { id: "click" };
-      const result = ag.machine.applyInput(input);
-      //TODO: handle this as an on-state-entry callback
-      if (result.isLegal && ag.machine.currentState.name === "acquiringTarget") {
-        ag.dest = randomScreenPos();
-      }
-      console.log({ result });
+      const input: AgentInput = keyIsDown(SHIFT) ? { id: "unleash" } : { id: "startReassign" };
+      ag.machine.applyInput(input);
     }
   });
 }
