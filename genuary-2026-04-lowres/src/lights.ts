af25cfd28e2c945f0d77c4a5c42932e92ff358ef
nbogie <neillbogie@googlemail.com>
2026-01-05 03:26:45 +0000

    lighting tweaks

--- genuary-2026-04-lowres/src/lights.ts
+++ genuary-2026-04-lowres/src/lights.ts
@@ -6,7 +6,7 @@ export function addLights(config: Config) {
     // directionalLight(color("#193937ff"), createVector(-1, 0.2, -0.3).normalize());
   }
   if (config.lighting.pinkAmbientLightEnabled) {
-    ambientLight(100, 20, 20);
+    ambientLight(90, 25, 25);
   }
   if (config.lighting.whiteDirectionalLightEnabled) {
     directionalLight(color("white"), createVector(1, 0.4, 0.3).normalize());
