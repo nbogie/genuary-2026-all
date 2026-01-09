56a49cd0f39ea03ce35d7bec9ced5f7ee4399bf1
nbogie <neillbogie@googlemail.com>
2026-01-09 06:00:00 +0000

    palettes, wireframe,

--- genuary-2026-08-city/src/main.ts
+++ genuary-2026-08-city/src/main.ts
@@ -6,11 +6,14 @@ p5.disableFriendlyErrors = true;
 type Config = ReturnType<typeof createConfig>;
 type PartialConfig = Partial<Config>;
 let config: Config;
+let palette: ReturnType<typeof createPalette>;
 
 window.setup = function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(2);
   config = createConfig();
+  palette = createPalette();
+  noLoop();
 };
 
 function createConfig() {
@@ -20,11 +23,13 @@ function createConfig() {
     fogDensity: 0.1,
     buildingWidth: 30,
     shouldRotateY: true as boolean | "mixed",
+    wireframe: random([true, false, false, false]),
+    shouldDrawFraming: false,
   };
 }
 
 window.draw = function draw() {
-  background(30);
+  background(palette.background);
   const numSkylines = 3;
   for (let ix = 0; ix < numSkylines; ix++) {
     const img = drawOneSkyline({
@@ -36,8 +41,23 @@ window.draw = function draw() {
     const h = map(ix, 0, numSkylines, 0, 1, true) * height;
     image(img, 0, h);
   }
+
+  if (config.shouldDrawFraming) {
+    drawFraming();
+  }
 };
 
+function drawFraming() {
+  //currently the skylines don't leave enough space to put this in
+  const gapWidth = 10;
+  const fakeHeight = height - gapWidth;
+  for (let i = 0; i <= 3; i++) {
+    const y = map(i, 0, 3, 0, fakeHeight, true);
+    fill(50);
+    noStroke();
+    rect(0, y, width, gapWidth);
+  }
+}
 function drawOneSkyline(configOverride: PartialConfig) {
   const layerG = createGraphics(width, height / 3, WEBGL);
   const finalG = createGraphics(width, height / 3);
@@ -53,16 +73,24 @@ function drawOneSkyline(configOverride: PartialConfig) {
     const z = map(ix, 0, numLayers - 1, -100, 300);
     drawOneLayerOfBuildingsOnto(layerG, z, configOverride);
 
-    const blurFraction = map(ix, 0, numLayers - 1, 0, 1);
-    const focusDepthFar = configOverride.focusDepthFar ?? config.focusDepthFar;
-    const blurAmount = 4 * (focusDepthFar ? blurFraction : 1 - blurFraction);
-
     layerG.push();
 
-    layerG.fill(30, config.fogDensity * 255);
+    const fogColour = color(palette.fogColour);
+    fogColour.setAlpha(config.fogDensity * 255);
+    layerG.fill(fogColour);
     layerG.noStroke();
-    layerG.rect(-width / 2, -height / 2, layerG.width, layerG.height);
-    layerG.filter(BLUR, blurAmount);
+    layerG.rect(-layerG.width / 2, -layerG.height / 2, layerG.width, layerG.height);
+
+    const blurFraction = map(ix, 0, numLayers - 1, 0, 1);
+    const focusDepthFar = config.wireframe
+      ? false
+      : configOverride.focusDepthFar ?? config.focusDepthFar;
+    const shouldBlur = !config.wireframe;
+    const blurAmount = 4 * (focusDepthFar ? blurFraction : 1 - blurFraction);
+
+    if (shouldBlur) {
+      layerG.filter(BLUR, blurAmount);
+    }
     layerG.pop();
     finalG.image(layerG, 0, 0);
   }
@@ -70,33 +98,45 @@ function drawOneSkyline(configOverride: PartialConfig) {
 }
 function drawOneLayerOfBuildingsOnto(g: p5.Graphics, z: number, partialConfig: PartialConfig) {
   push();
+  g.push();
   const worldWidth = 500;
 
   const layerXOffset = randomGaussian(0, 5);
-  g.translate(layerXOffset, 20 - z / 10, 0);
+  g.translate(layerXOffset, 50 + z / 10, 0);
   for (let x = -worldWidth; x < worldWidth; x += config.buildingWidth * 1.1) {
     g.push();
     const maxBuildingHeight = config.buildingWidth * 5;
     const y = random([0, 0, random(0.1, 1) * maxBuildingHeight]);
     if (y) {
       g.translate(x, -y / 2, z);
-      const colour = pickBiased(palette.colors, 0.8);
-      g.ambientMaterial(colour);
       const w = config.buildingWidth;
-      g.noStroke();
       // g.rotateX(random(-1, 1) * 0.1);
       // g.rotateZ(random(-1, 1) * 0.02);
       if (partialConfig.shouldRotateY === true) {
         g.rotateY(random(-1, 1) * 0.9);
       }
+
+      if (config.wireframe) {
+        g.stroke(palette.stroke);
+        g.noFill();
+      } else {
+        const colour = pickBiased(palette.colors, 0.8);
+        g.ambientMaterial(colour);
+        g.noStroke();
+      }
+
       g.box(w, y, w);
     }
 
     g.pop();
   }
+  g.pop();
   pop();
   return g;
 }
+window.windowResized = function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+};
 
 window.keyPressed = function keyPressed() {
   if (key === "f") {
@@ -107,6 +147,8 @@ window.keyPressed = function keyPressed() {
   if (key === " ") {
     config.seed = millis();
     config.shouldRotateY = random([true, false, "mixed", "mixed"]);
+    config.wireframe = random([true, false]);
+    palette = createPalette();
 
     redraw();
   }
@@ -133,11 +175,27 @@ function pickBiased<T>(arr: T[], decay = 0.5): T {
   return arr[indexCapped];
 }
 
-const palette = {
-  name: "book",
-  colors: ["#1c2738", "#d8b1a5", "#c95a3f", "#d1a082", "#037b68", "#be1c24"],
-  stroke: "#0e0f27",
-  background: "#f5b28a",
-  size: 6,
-  type: "chromotome",
-};
+function createPalette() {
+  const palette1 = {
+    name: "book",
+    colors: ["#1c2738", "#d8b1a5", "#c95a3f", "#d1a082", "#037b68", "#be1c24"],
+    stroke: "#0e0f27",
+    background: "#f5b28a",
+    fogColour: "#1e1e1e",
+    size: 6,
+    type: "chromotome",
+  };
+  const paletteBlueprint = (() => {
+    const bg = random(["dodgerblue", "#1c2738", "#1c2738", "#1c2738"]);
+    return {
+      name: "blueprint-book",
+      colors: ["#1c2738", "#d8b1a5", "#c95a3f", "#d1a082", "#037b68", "#be1c24"],
+      stroke: "white",
+      background: bg,
+      fogColour: bg,
+      size: 6,
+      type: "chromotome",
+    };
+  })();
+  return random([palette1, paletteBlueprint]);
+}
