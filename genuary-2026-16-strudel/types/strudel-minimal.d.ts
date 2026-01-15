8a92bc540312933a26c36cde40f1593d467a6fdc
nbogie <neillbogie@googlemail.com>
2026-01-15 10:36:29 +0000

    add fluent interface to strudel type stub

--- genuary-2026-16-strudel/types/strudel-minimal.d.ts
+++ genuary-2026-16-strudel/types/strudel-minimal.d.ts
@@ -5,14 +5,43 @@ interface FakeStrudel {
     hush: any;
     pure: any;
 }
+
 declare var strudel: FakeStrudel;
 
-declare var n: any;
-declare var note: any;
-declare var s: any;
-declare var samples: any;
-declare var stack: any;
+type FluentFn = (...args: any[]) => Fluent;
+type SignalFn = (...args: any[]) => Signal;
+
+declare var n: FluentFn;
+declare var note: FluentFn;
+declare var s: FluentFn;
+declare var stack: FluentFn;
 declare var saw: any;
+declare var samples: any;
+declare var perlin: SignalFn;
+
+interface Signal {
+    slow: SignalFn;
+    fast: SignalFn;
+    range: SignalFn;
+}
+
+interface Fluent {
+    clip: FluentFn;
+    cpm: FluentFn;
+    cutoff: FluentFn;
+    delay: FluentFn;
+    fast: FluentFn;
+    gain: FluentFn;
+    lpf: FluentFn;
+    lpq: FluentFn;
+    slow: FluentFn;
+    note: FluentFn;
+    play: () => void;
+    room: FluentFn;
+    s: FluentFn;
+    sound: FluentFn;
+    sz: FluentFn;
+}
 
 //if this becomes a module rather than a script, we'd need:
 // declare global {
