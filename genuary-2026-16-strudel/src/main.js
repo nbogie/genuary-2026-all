8b9e9ad7c848fa1f3fcd688e963ef750aebd55cd
nbogie <neillbogie@googlemail.com>
2026-01-15 03:27:31 +0000

    minimum from my openprocessing strudel integration experiment

--- genuary-2026-16-strudel/src/main.js
+++ genuary-2026-16-strudel/src/main.js
@@ -1,21 +1,63 @@
+p5.disableFriendlyErrors = true;
+
+let myInputs;
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(windowWidth, windowHeight);
+    setupMyInputs();
+    //@ts-ignore
+    strudel.initStrudel({
+        //@ts-ignore
+        prebake: () => samples("github:tidalcycles/dirt-samples"),
+    });
+    frameRate(30);
 }
 
 function draw() {
-    const cellSize = 120;
-    const diameter = random(30, 120);
-    const x = snapTo(random(width), cellSize);
-    const y = snapTo(random(height), cellSize);
-    noStroke();
-    fill(snapTo(random(255), 50));
-    circle(x, y, diameter);
+    background(20);
 }
 
-/**
- * @param {number} value
- * @param {number} increment
- */
-function snapTo(value, increment) {
-    return increment * round(value / increment);
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function keyPressed() {
+    if (key === " ") {
+        //simplest example
+        //@ts-ignore
+        s("bd hh sd hh [bd bd] hh sd hh").play();
+    }
+
+    if (key === ".") {
+        //@ts-ignore
+        strudel.hush();
+    }
+
+    if (key >= "0" && key <= "9") {
+        const num = parseInt(key) - 1;
+
+        const pattn = myPatterns[num];
+        if (!pattn) {
+            console.log(
+                "no pattern in slot: " + num + " so ignoring keypress."
+            );
+            return;
+        }
+
+        console.log("evaluating strudel code #" + num + ": " + pattn.title);
+        pattn.fn();
+    }
+}
+
+function setupMyInputs() {
+    //strudel already provides "mousex", but that is for the window not the canvas. we have more control here.
+
+    //@ts-ignore
+    const mouseXInput = strudel.pure("unused").withValue((val) => {
+        return map(mouseX, 0, width, 0, 1, true);
+    });
+
+    myInputs = {
+        mouseX: mouseXInput,
+    };
 }
---
+++ genuary-2026-16-strudel/src/patterns.js
@@ -0,0 +1,50 @@
+//@ts-nocheck
+const myPatterns = [
+    {
+        title: [
+            "using custom inputs",
+            "mouse x is drum lpf freq",
+            "wanderer x is lpf freq for synth",
+            "wanderer y is gain for drums",
+        ].join("\n"),
+        fn: () =>
+            stack(
+                n("[0 0*2, 2] [1 - - <1*6 1>] [4 2] 3*6")
+                    .sound("jazz")
+                    .lpf(myInputs.mouseX.range(100, 4000))
+                    .lpq(10)
+                    // .gain(myInputs.wandererY.range(0, 0.7))
+                    .delay(1),
+                stack(
+                    note(
+                        "<~ <[d4 a3 d3] [d e] [a g]> f g <[c4 d4] a a> d e f g a <c4 b [c4 d4]> <g c> a g f e>*16"
+                    ),
+                    note("<d2 a2 c2 f2>*1.75")
+                )
+                    // .cutoff(myInputs.wandererX.range(0, 4000))
+                    .gain(0.35)
+                    .s("supersaw")
+                    .delay(0.1)
+            )
+                .cpm(120 / 4)
+                .room(3)
+                .sz(0.4)
+                .play(),
+    },
+
+    {
+        title: ["chopping a beat"].join("\n"),
+        fn: () =>
+            stack(
+                n("[0 0*2, 2] [1 - - <1*6 1>] [4 2] 3*6")
+                    .sound("jazz")
+                    .lpf(myInputs.mouseX.range(100, 4000))
+                    .lpq(10)
+                    .delay(1)
+            )
+                .cpm(120 / 4)
+                .room(3)
+                .sz(0.4)
+                .play(),
+    },
+];
