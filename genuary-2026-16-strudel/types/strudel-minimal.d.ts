19787421978387b850212e16698291e6d2ece9e6
nbogie <neillbogie@googlemail.com>
2026-01-16 01:18:52 +0000

    more types

--- genuary-2026-16-strudel/types/strudel-minimal.d.ts
+++ genuary-2026-16-strudel/types/strudel-minimal.d.ts
@@ -18,6 +18,11 @@ declare var stack: FluentFn;
 declare var saw: any;
 declare var samples: any;
 declare var perlin: SignalFn;
+declare var ply: FluentFn;
+declare var mouseY: FlentFn;
+declare var mouseX: FlentFn;
+declare var speed: FlentFn;
+declare var mul: FlentFn;
 
 interface Signal {
     slow: SignalFn;
@@ -26,21 +31,31 @@ interface Signal {
 }
 
 interface Fluent {
+    chop: FluentFn;
     clip: FluentFn;
     cpm: FluentFn;
+    cut: FluentFn;
     cutoff: FluentFn;
     delay: FluentFn;
     fast: FluentFn;
+    fit: FluentFn;
     gain: FluentFn;
     lpf: FluentFn;
     lpq: FluentFn;
-    slow: FluentFn;
     note: FluentFn;
     play: () => void;
+    rarely: FluentFn;
     room: FluentFn;
     s: FluentFn;
+    slice: FluentFn;
+    slow: FluentFn;
     sound: FluentFn;
     sz: FluentFn;
+    sometimesBy: FlentFn;
+    range: FlentFn;
+    ply: FlentFn;
+    gain: FlentFn;
+    transpose: FlentFn;
 }
 
 //if this becomes a module rather than a script, we'd need:
