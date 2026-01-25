4eed51a0641a21d26f82aa897ce215aecbdffbdb
nbogie <neillbogie@googlemail.com>
2026-01-25 12:57:14 +0000

    adding project somedays' header

---
+++ genuary-2026-25-organic-geometry/src/header.ts
@@ -0,0 +1,24 @@
+/*
+| Author          | Neill Bogie
+| Title           | WCCChallenge 2026 Week 4 + Genuary day 25: "Organic Geometry"
+| 📅 Started      | 2026-01-25
+| 📅 Completed    | 2026-01-25
+| 🕒 Taken        | ~2.5hrs                       
+| 🤯 Concept      | Tentacles composed only of circles that reach towards the moving mouse, with noise
+| 🔎 Focus        | From-memory algorithm practice.  p5v2 + TypeScript practice.
+| 🙏 Credits      | 1. I learned this in 2019 from Daniel Shiffman's Inverse Kinematics challenge: 
+|                 |    https://www.youtube.com/watch?v=hbgDqyy8bIw
+|                 |    although I might have butchered the algorithm, here.
+|                 | 2. Project Somedays for this neat header format!
+| 💾 Previously   | I originally implemented the same idea in 2019 - https://github.com/nbogie/p5js-ik-tentacles 
+|                 | I didn't look at the code or the alg.
+| 🤖 AI usage     | none
+
+Made for Sableraph's weekly creative coding challenges, reviewed weekly on https://www.twitch.tv/sableraph
+See other submissions here: https://openprocessing.org/curation/78544
+Join The Birb's Nest friendly Discord community! https://discord.gg/g5J6Ajx9Am
+*/
+
+//this source code is transpiled from typescript, so it may be hard to read.
+//The original typescript source is at:
+//https://github.com/nbogie/genuary-2026-all/tree/main/genuary-2026-25-organic-geometry
